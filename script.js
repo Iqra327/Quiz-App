c6f842bc93a3c0d66414549532a0769b35b58d53
Iqra327 <iqrarasheed219@gmail.com>
2024-08-07 17:09:02 +0500

    Quiz-app, all-done

--- script.js
+++ script.js
@@ -1,25 +1,4 @@
-const quiz = [
-  {
-      question: "Q. Which of the following is not a CSS box model property?",
-      choices: ["margin", "padding", "border-radius", "border-collapse"],
-      answer: "border-collapse"
-  },
-  {
-      question: "Q. Which of the following is not a valid way to declare a function in JavaScript?",
-      choices: ["function myFunction() {}", " let myFunction = function() {};", "myFunction: function() {}", "const myFunction = () => {};"],
-      answer: "myFunction: function() {}"
-  },
-  {
-      question: "Q. Which of the following is not a JavaScript data type?",
-      choices: ["string", "boolean", "object", "float"],
-      answer: "float"
-  },
-  {
-      question: "Q. What is the purpose of the this keyword in JavaScript?",
-      choices: ["It refers to the current function.", "It refers to the current object.", "It refers to the parent object.", " It is used for comments."],
-      answer: "It refers to the current object."
-  }
-];
+import { quiz } from "./data.js";
 
 let currentIndex = 0;
 let score = 0;
@@ -99,6 +78,7 @@ function displayAlert(msg) {
     }, 2000);
 };
 
+//quiz timer
 let timeInterval;
 const timerDisplay = document.querySelector('.js-timer');
 function startTimer(){
